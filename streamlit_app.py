80014df172a08e7131eeeeff06d267ba90ec8993
William Schmitt <will@streamlit.io>
2021-03-26 14:35:32 -0700

    adds some graphs and requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,25 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import numpy as np
 from eng_to_ipa import convert
 import pandas as pd
 import re
+import difflib
+
 
 st.markdown("""
-# Trash Talk
+# New-ish English
+A getting started kit for creating your English spin-off language.
+""")
+st.components.v1.html("""
+<iframe src='https://gfycat.com/ifr/AlienatedSeparateHermitcrab' frameborder='0' scrolling='no' allowfullscreen width='640' height='300'></iframe><p> <a href="https://gfycat.com/alienatedseparatehermitcrab">via Gfycat</a></p>img src="https://gfycat.com/alienatedseparatehermitcrab"/>
+""")
 
-a game of Language construction, based on sources from:
-*
+st.markdown("""
+Writing a new language is fun, but challenging endeavour!
+It's not the highest art-form, but it's an rewarding creative writing exercise.
+This app is designed to get you started on defining phonological changes for what
+English could sound like hundreds of years in the future.
 """)
 
 sample_passage = """It began with the forging of the Great Rings. Three were given to the Elves, immortal, wisest and fairest of all beings. Seven to the Dwarf-Lords, great miners and craftsmen of the mountain halls. And nine, nine rings were gifted to the race of Men, who above all else desire power. For within these rings was bound the strength and the will to govern each race. But they were all of them deceived, for another ring was made. Deep in the land of Mordor, in the Fires of Mount Doom, the Dark Lord Sauron forged a master ring, and into this ring he poured his cruelty, his malice and his will to dominate all life.
@@ -32,59 +43,84 @@ def canadian_raising(l):
     final_vowel = 'u'
     l = [w.replace(initial_vowel, final_vowel) for w in l]
     return l
-    # obstruents = 'ptksfbdg'.split('')
-    # initial_vowel = 'aʊ'
-    # final_vowel = 'u'
-    # def transform(w):
-        # for ob in obstruents:
-            # if ob+initial_vowel in w:
-                # return w.replace(ob+initial_vowel, ob+final_vowel)
 
-    # return [transform(w) for w in l]
 def non_rhotic(l):
     return [w.replace('r', '') for w in l]
 
 def final_rhotic(l):
     return [re.sub(r'ə$', 'ər:', w) for w in l]
 
-def th_retroflex(l):
-    return [w.replace('θ', 'ɖ') for w in l]
+def drop_schwas(l):
+    return [re.sub(r'ə', '', w) for w in l]
 
-st.markdown("## How will your language evolve?")
+def th_retroflex(l):
+    return [w.replace('t', 'ɖ') for w in l]
+
+
+st.markdown("""
+## How will your language evolve?
+Choose some traits below to add to your lanague
+""")
 mutations = {
     "Canadian Vowel Raising": [canadian_raising, 'https://en.wikipedia.org/wiki/Canadian_raising', "All speakers kneel at the alter of Tim Horton's and pronounce it as 'aboot'"],
     "Glottal Stops": [glottal_stops, "https://en.wikipedia.org/wiki/Glottal_stop", "Speak with your mouth *and* your throat!"],
     "Non Rhotic": [non_rhotic, "https://www.thoughtco.com/rhoticity-speech-4065992", "Boston ascends! With Irish-American accents at the center of your civilization nobody will pronounce any of their Rs"],
-    "Final Rhotic": [final_rhotic, "https://www.thoughtco.com/rhoticity-speech-4065992", "The Pirates of the Carribean sequels are actually good! Their speach patterns echo through your civilization."]
-    "Add Retroflex D": [th_retroflex, "https://en.wikipedia.org/wiki/Retroflex_consonant", "Join the Indian Sprachbund! Add sophistication to your consonants and watch tongues do backflips"]
+    "Final Rhotic": [final_rhotic, "https://www.thoughtco.com/rhoticity-speech-4065992", "The Pirates of the Carribean sequels are actually good! Their speach patterns echo through your civilization."],
+    "Add Retroflex D": [th_retroflex, "https://en.wikipedia.org/wiki/Retroflex_consonant", "Join the Indian Sprachbund! Add sophistication to your consonants and watch tongues do backflips"],
+    "Drop all Schwas ('uh', /ə/)": [drop_schwas, "https://en.wikipedia.org/wiki/Schwa", "Go nuclear and eliminate an entire sound from the language"]
 }
 
 choices = {}
 for m in mutations:
     choices[m] = st.checkbox(m, help=mutations[m][1])
+    st.write(mutations[m][-1])
 
 generations = [gen0_tokens, gen0_ipa]
 for c,ok in choices.items():
     if not ok: continue
     tf = mutations[c][0]
     generations.append(tf(generations[-1]))
-st.markdown("## IPA")
+st.markdown("""
+## How will it sound?
+In the table below you can see how each generation of these changes affects a generation of words.
+""")
 generation_columns = ["0th Gen Text"]
 generation_columns.extend([str(x) + "th Gen IPA" for x in range(0, len(generations)-1)])
 
 table = pd.DataFrame(pd.DataFrame( generations)).transpose()
 table.columns = generation_columns
-table
+st.write(table.transpose())
 
 st.markdown("## Behold!")
-col1, col2 = st.beta_columns(2)
-' '.join(generations[-1])
+
+st.write(' '.join(generations[-1]))
+
 text = ' '.join(generations[-1][0:20])
 components.iframe("http://ipa-reader.xyz/?text=" + text, height=400)
 
+# TODO
+passage_len = len(gen0_ipa)
+genDistFromPrev = [0]
+
+for i in range(2, len(generations)):
+    distances = [ difflib.SequenceMatcher(None, generations[i - 1][wi], generations[i][wi]).ratio() for wi in range(1, passage_len)]
+    genDistFromPrev.append(np.mean(distances))
+
+genDistFrom0 = [0]
+for i in range(2, len(generations)):
+    distances = [ difflib.SequenceMatcher(None, generations[1][wi], generations[i][wi]).ratio() for wi in range(1, passage_len)]
+    genDistFrom0.append(np.mean(distances))
+
+
+st.markdown("## How dissimlar is it from English? (Distance v Generation)")
+st.markdown("Use the chart below to track your progress! Since we've only changed the phonology so far, the output is still probably a dialect of English, but it's cool nonetheless!")
+st.line_chart(pd.DataFrame(zip(genDistFrom0[1:], genDistFromPrev[1:]), columns=["Distance From English", "Distance From Previous"]))
+st.markdown("""This is just the ratio of simlarity, a smarter way to do this would be to use feature vectors and informatin gain to do a weighted analysis but that's outside scope here.
+
+https://www.aclweb.org/anthology/W97-1102.pdf""")
 
 st.markdown("""
-### What's next?
+## What's next?
 Thankfully, languages are much more than a bag of sounds!
 As English ages in your theoretical world, it will experience
 * Semantic changes to existing words,
@@ -94,8 +130,9 @@ As English ages in your theoretical world, it will experience
 * Changes to the existing writing system.
 
 ### References:
-* Core exercise borrowed from https://www.linguisticsociety.org/sites/default/files/02e_92.3Sanders.pdf
-* https://www.zompist.com/kitlong.html
-* Lots of wikipedia,
-"""
-)
+1. Core exercise borrowed from an article in the [Linguistic Society](https://www.linguisticsociety.org/sites/default/files/02e_92.3Sanders.pdf)
+1. [The Conlang Construction Toolkit](https://www.zompist.com/kitlong.html)
+""")
+st.components.v1.html("""
+<iframe src="https://giphy.com/embed/zGnnFpOB1OjMQ" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/lord-of-the-rings-hobbits-shire-zGnnFpOB1OjMQ">via GIPHY</a></p>
+""")
